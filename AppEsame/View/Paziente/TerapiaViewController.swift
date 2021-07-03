b7281ca82bbbc5f93f785d64133a67d459d5748a
Annanna985 <ann.cervo@studenti.unina.it>
2021-07-03 17:46:08 +0200

    Aggiunto generazione pdf

--- AppEsame/View/Paziente/TerapiaViewController.swift
+++ AppEsame/View/Paziente/TerapiaViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class TerapiaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var medicinali = ["Tachipirina", "Cardiospirina", "Brufen"]
     var ore = [1,2,1]
+  
     @IBOutlet weak var terapiaTableView: UITableView!
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return medicinali.count
@@ -32,9 +33,32 @@ class TerapiaViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
 //        DA TOGLIERE IL COMMENTO QUANDO SI VOGLIONO ELIMINARE LE NOTIFICHE
 //        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func condividiPDF(_ sender: Any) {
+        // 1
+        guard
+            let title = "Terapia" as? String,
+          let medicinale = medicinali as? [String],
+          let descrizione = "Descrizione"as? String
+          else {
+            // 2
+            let alert = UIAlertController(title: "All Information Not Provided", message: "You must supply all information to create a flyer.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+              // 3
+              let pdfCreator = PDFCreator(title: title, medicinale: medicinale, descrizione: descrizione)
+              let pdfData = pdfCreator.createFlyer()
+              let vc = UIActivityViewController(activityItems: [pdfData], applicationActivities: [])
+              present(vc, animated: true, completion: nil)
     }
     
 }
