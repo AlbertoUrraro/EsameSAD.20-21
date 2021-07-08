4c9d64c821a6c54844b79b84ade8a828f4646a72
AlbertoUrraro <81329291+AlbertoUrraro@users.noreply.github.com>
2021-07-08 16:40:25 +0200

    immagine Profilo paziente/medico

--- AppEsame/View/Medico/ProfiloMedicoViewController.swift
+++ AppEsame/View/Medico/ProfiloMedicoViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 import Firebase
-class ProfiloMedicoViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+
+class ProfiloMedicoViewController:  UIViewController, UITableViewDelegate, UICollectionViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
    
@@ -21,16 +23,41 @@ class ProfiloMedicoViewController:  UIViewController, UITableViewDelegate, UITab
     
     var titoli = ["Utente","Posizione","Applicazione"]
     
-   
+    var imagePicker: UIImagePickerController!
+    
+    
+    
     override func viewDidLoad() {
             super.viewDidLoad()
             title="Profilo"
         profiloMedico.delegate = self
         profiloMedico.dataSource = self
-            
+        
+        immagineProfilo.image = UIImage(named: "user")
+        //immagineProfilo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        immagineProfilo.layer.cornerRadius = immagineProfilo.bounds.height/2
+        immagineProfilo.contentMode = .scaleToFill
+        immagineProfilo.layer.borderWidth = 3
+        immagineProfilo.layer.borderColor = UIColor.black.cgColor
+        let imageClick = UITapGestureRecognizer(target: self, action: #selector(apriImagePicker))
+        immagineProfilo.addGestureRecognizer(imageClick)
+        immagineProfilo.clipsToBounds = true
+        modificaFoto.addTarget(self, action: #selector(apriImagePicker), for: .touchUpInside)
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
             
         }
 
+   
+    
+    
+    
+    
+    
+    
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return info[section].count
@@ -51,6 +78,8 @@ class ProfiloMedicoViewController:  UIViewController, UITableViewDelegate, UITab
         }
                       
     }
+    
+    
     
     
     
@@ -81,5 +110,42 @@ class ProfiloMedicoViewController:  UIViewController, UITableViewDelegate, UITab
 
         
     }
+    
+    @IBOutlet weak var immagineProfilo: UIImageView!
+    @IBOutlet weak var modificaFoto: UIButton!
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @objc  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.editedImage.rawValue) ] as? UIImage{
+            self.immagineProfilo.image = pickedImage
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    @objc func apriImagePicker(_sender:Any){
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    //funzione caricamento immagine profilo
+    
+    /*func uploadImmagine(_image:UIImage, completion: @escaping ((_ url: String?)->())){
+        guard let uid = Auth.auth().currentUser?.uid else{return}
+        let memoriaStorege = Storage.storage().reference().child("utente/\(uid)")
+        //guard let imageData = image.jpegData(compressionQuality: 0.75) else{
+          //  return
+        }*/
+    
+        //memoriaStorege.putData(<#T##uploadData: Data##Data#>)
+    
+    
     
 }
