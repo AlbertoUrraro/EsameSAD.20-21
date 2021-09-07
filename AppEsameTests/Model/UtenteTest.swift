690c422677a8487a593076dba3090858ac7e358a
Annanna985 <ann.cervo@studenti.unina.it>
2021-09-07 17:55:11 +0200

    Completamento test utente

--- AppEsameTests/Model/UtenteTest.swift
+++ AppEsameTests/Model/UtenteTest.swift
@@ -22,14 +22,59 @@ class UtenteTest: XCTestCase {
         super.tearDown()
 }
     
+    func testID(){
+        XCTAssertEqual(utente.id, "01234")
+    }
+    
+    func testuid(){
+        XCTAssertEqual(utente.uid,"0245")
+    }
+    
+    
     func testNome(){
         XCTAssertEqual(utente.nome, "Marco")
     }
     
+    func testCognome(){
+        XCTAssertEqual(utente.cognome, "Rossi")
+    }
+    
+    func testDataNascita(){
+        XCTAssertEqual(utente.dataNascita, "01/01/1970")
+    }
+    
+    func testCF(){
+        XCTAssertEqual(utente.codiceFiscale,"ABC123")
+    }
+    
+    func testTelefono(){
+        XCTAssertEqual(utente.telefono,"123456789")
+    }
+    
+    func testEmail(){
+        XCTAssertEqual(utente.email,"marco.rossi@gmail.com")
+    }
+    
+    func testTipo(){
+        XCTAssertEqual(utente.tipo,"Paziente")
+    }
+    
+    func testPassword(){
+        XCTAssertEqual(utente.password,"marcorossi")
+    }
+    func testIndirizzo(){
+        XCTAssertEqual(utente.indirizzo,"Via Milano 88")
+    }
+    
+    func testCitta(){
+        XCTAssertEqual(utente.citta,"Napoli")
+    }
+  
     func testCap(){
         XCTAssertEqual(utente.cap,"80000")
     }
     
+   
     
     
     
